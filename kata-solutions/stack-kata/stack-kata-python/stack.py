32c1f57fcf874e40ce6e87ae674f2b8ff439ec95
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-05-29 21:08:46 +0200

    Solution stack kata

--- kata-solutions/stack-kata/stack-kata-python/stack.py
+++ kata-solutions/stack-kata/stack-kata-python/stack.py
@@ -1,2 +1,17 @@
 class Stack:
-  pass
+  def __init__(self):
+    self.size = 0
+    self.element = [-1] * 2
+
+  def is_empty(self) -> bool:
+    return self.size == 0
+
+  def pop(self) -> int:
+    if self.is_empty() == True:
+      raise KeyError("Stack underflow")
+    self.size -= 1
+    return self.element[self.size]
+
+  def push(self, value:int) -> None:
+    self.element[self.size] = value
+    self.size += 1
--- kata-solutions/stack-kata/stack-kata-python/stack_spec.py
+++ kata-solutions/stack-kata/stack-kata-python/stack_spec.py
@@ -5,5 +5,44 @@ from stack import Stack
 
 with description(Stack) as self:
   with context("Given a new stack"):
-    with it("True equals False"):
-      expect(True).to(be_false)
+    with before.each:
+      self.my_stack = Stack()
+    
+    with it("contains no elements"):
+      expect(self.my_stack.is_empty()).to(be_true)
+
+    with it("throws an exception on a pop operation"):
+      expect(lambda: self.my_stack.pop()).to(raise_error(KeyError, "Stack underflow"))
+
+    with context("with one item pushed"):
+      with before.each:
+        self.my_stack.push(8)
+
+      with it("is not empty anymore"):
+        expect(self.my_stack.is_empty()).to(be_false)
+
+      with context("when element is popped"):
+        with it("contains no more elements"):
+          self.my_stack.pop()
+          expect(self.my_stack.is_empty()).to(be_true)
+    
+        with it("returns the popped element"):
+          expect(self.my_stack.pop()).to(be(8))
+
+      with context("when an additional element is pushed"):
+        with before.each:
+          self.my_stack.push(9)
+
+        with it("is not empty after a single pop"):
+          self.my_stack.pop()
+          expect(self.my_stack.is_empty()).to(be_false)
+          
+        with it("returns the most recent element after one pop"):
+          expect(self.my_stack.pop()).to(be(9))
+
+        with it("returns the first element after two pops"):
+          self.my_stack.pop()
+          expect(self.my_stack.pop()).to(be(8))
+
+
+
