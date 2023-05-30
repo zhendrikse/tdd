0ed8f40a8eecc1aced93823347445bb71e776394
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-05-30 10:04:54 +0200

    Solution stack kata java

---
+++ kata-solutions/stack-kata/stack-kata-java/src/main/java/Stack.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Stack {
+  private Plate[] plates = new Plate[2];
+  int size = 0;
+  
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public void push(Plate plate) {
+     
+     this.plates[this.size++] = plate;
+  }
+
+  public Plate pop() {
+     if (isEmpty()) {
+       throw new RuntimeException("Stack underflow");
+     }
+    
+     return this.plates[--this.size];
+  }
+}
---
+++ kata-solutions/stack-kata/stack-kata-java/src/test/java/StackSpec.java
@@ -0,0 +1,65 @@
+import com.mscharhag.oleaster.runner.OleasterRunner;
+import org.junit.runner.RunWith;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.*;
+import static com.mscharhag.oleaster.matcher.Matchers.*;
+
+@RunWith(OleasterRunner.class)
+public class StackSpec {
+  private Stack myStack;
+  private Plate myPlate;
+  {
+    describe("A new stack", () -> {
+
+      beforeEach(() -> {
+        myStack = new Stack();
+      });
+
+      it("is empty", () -> {
+        expect(myStack.isEmpty()).toBeTrue();
+      });
+      
+      it("throws an exception when a pop operation is applied", () -> {
+        expect( () -> { myStack.pop(); } ).toThrow(RuntimeException.class);
+      });
+
+    });
+
+    describe("A new stack with one plate pushed", () -> {
+
+      beforeEach(() -> {
+        myStack = new Stack();
+        myPlate = new Plate("plate1");
+        myStack.push(myPlate);
+      });
+
+      it("is not empty", () -> {
+        expect(myStack.isEmpty()).toBeFalse();
+      });
+
+      it("returns the plate after a pop operation", () -> {
+        expect(myStack.pop()).toEqual(myPlate);
+      });
+
+      it("is empty after a pop operation", () -> {
+        myStack.pop();
+        expect(myStack.isEmpty()).toBeTrue();
+      });
+
+      it("is not empty after an addition push and a subsequent pop", () -> {
+         myStack.push(new Plate("plate2"));
+         myStack.pop();
+         expect(myStack.isEmpty()).toBeFalse();
+      });
+
+      it("returns the first plate after an additional push and two pops", () -> {
+         Plate plate2 = new Plate("plate2");
+         myStack.push(plate2);
+        
+         myStack.pop();
+         Plate returnedPlate = myStack.pop();
+        expect(returnedPlate).toEqual(myPlate);
+      });
+    });
+
+  }
+}
