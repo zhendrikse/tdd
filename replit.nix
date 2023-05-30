5450b1343fd6215ab22f2742259dcada14b2e7fc
Replit user <>
2023-05-30 11:23:57 +0000

    Added nix

---
+++ replit.nix
@@ -0,0 +1,22 @@
+{ pkgs }: {
+  deps = [
+    pkgs.vim
+    pkgs.clojure
+    pkgs.clojure-lsp
+    pkgs.leiningen
+    pkgs.graalvm17-ce
+		pkgs.maven
+		pkgs.replitPackages.jdt-language-server
+		pkgs.replitPackages.java-debug
+    pkgs.python39Full
+    pkgs.replitPackages.prybar-python3
+    pkgs.python39Packages.pip
+    pkgs.python39Packages.poetry
+    pkgs.nodejs-14_x
+		pkgs.nodePackages.typescript-language-server
+		pkgs.yarn
+		pkgs.replitPackages.jest
+  ];
+  PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+  LANG = "en_US.UTF-8";
+}
