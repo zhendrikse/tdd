bcb09b98a2f2bfe198027e75f1b301957cc13f4c
Replit user <>
2023-05-31 11:23:48 +0000

    Fixtures

--- tdd-katas/video-store-kata/video-store-python/video_store_test.py
+++ tdd-katas/video-store-kata/video-store-python/video_store_test.py
@@ -3,24 +3,24 @@ from customer import Customer
 from rental import Rental
 from movie  import Movie, MovieType
 
-def testSingleNewReleaseStatement():
-    customer = Customer("Fred")
+@pytest.fixture
+def customer():
+  return  Customer("Fred")
+
+def testSingleNewReleaseStatement(customer):
     customer.add_rental(Rental(Movie("The Cell", MovieType.NEW_RELEASE), 3))
     assert "Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n" == customer.statement()
 
-def test_DualNewReleaseStatement():
-    customer = Customer("Fred")
+def test_DualNewReleaseStatement(customer):
     customer.add_rental(Rental(Movie("The Cell", MovieType.NEW_RELEASE), 3))
     customer.add_rental(Rental(Movie("The Tigger Movie", MovieType.NEW_RELEASE), 3))
     assert "Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n" == customer.statement()
   
-def test_SingleChildrensStatement():
-    customer = Customer("Fred")
+def test_SingleChildrensStatement(customer):
     customer.add_rental(Rental(Movie("The Tigger Movie", MovieType.CHILDRENS), 3))
     assert "Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n" == customer.statement()
 
-def test_MultipleRegularStatement():
-    customer = Customer("Fred")
+def test_MultipleRegularStatement(customer):
     customer.add_rental(Rental(Movie("Plan 9 from Outer Space", MovieType.REGULAR), 1))
     customer.add_rental(Rental(Movie("8 1/2", MovieType.REGULAR), 2))
     customer.add_rental(Rental(Movie("Eraserhead", MovieType.REGULAR), 3))
