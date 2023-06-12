b9414775b8ee7432978b244cddba71bab3075a4b
Replit user <>
2023-06-12 16:33:28 +0000

    Removed cookie cutter templates

--- cookiecutter-templates/cookiecutter-kata-javascript/{{ cookiecutter.kata|lower }}/spec/{{ cookiecutter.kata }}_spec.js
+++
@@ -1,9 +0,0 @@
-import { {{  cookiecutter.kata }} } from '../src/{{ cookiecutter.kata }}.js';
-
-describe("A new {{ cookiecutter.kata }}", function() {
-    it("is successfully created", function () {
-        var {{ cookiecutter.kata|lower }}  = new {{  cookiecutter.kata }}()
-        expect(false).toEqual(true);
-    })
-})
-
