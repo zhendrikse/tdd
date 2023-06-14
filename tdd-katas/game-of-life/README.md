8987655ca8d2d56d0b1d87e996d4dbe608b7e505
Replit user <>
2023-06-14 15:07:23 +0000

    Rely on cookiecutter

---
+++ tdd-katas/game-of-life/README.md
@@ -0,0 +1,3 @@
+# Game of Life
+
+Conway's game of life.
--- tdd-katas/game-of-life/game-of-life-python/game_of_life_spec.py
+++
@@ -1,9 +0,0 @@
-from mamba import description, it, context, before
-from expects import expect, equal, raise_error, be_false, be_true
-#from game_of_life import living_cell, dead_cell, Game
-  
-with description("Game of life") as self:
-  with context("Living cell survival"):
-    
-    with it("dies on one living neighbour"):
-      expect(True).to(be_false)
