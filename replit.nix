9aca7def200d698d4ebebb4d56d082746e422d87
Replit user <>
2023-06-14 09:19:33 +0000

    Added pipenv

--- replit.nix
+++ replit.nix
@@ -8,6 +8,8 @@
 		pkgs.maven
 		pkgs.replitPackages.jdt-language-server
 		pkgs.replitPackages.java-debug
+    pkgs.pipenv
+    pkgs.pipreqs
     pkgs.python310Full
     pkgs.replitPackages.prybar-python310
     pkgs.replitPackages.stderred
@@ -23,6 +25,9 @@
     pkgs.nodePackages.typescript
     pkgs.nodePackages.typescript-language-server
   ];
-  PYTHONBIN = "${pkgs.python10Full}/bin/python3.10";
+  PYTHONHOME = "${pkgs.python310Full}";
+  PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
   LANG = "en_US.UTF-8";
+  STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
+  PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
 }
