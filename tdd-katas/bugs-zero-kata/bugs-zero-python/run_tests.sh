403e72598f624bc1c0cc34f37c1c9828c4057fca
Replit user <>
2023-06-20 18:30:05 +0000

    Repaired bugs zero

--- tdd-katas/bugs-zero-kata/bugs-zero-python/run_tests.sh
+++ tdd-katas/bugs-zero-kata/bugs-zero-python/run_tests.sh
@@ -1,2 +1,2 @@
 PYTHONPATH=$PYTHONPATH:src:test
-poetry run pytest && poetry run coverage run --branch --source gilded_rose gilded_rose_test.py && poetry run coverage report -m && poetry run coverage html
+poetry run coverage run game_test.py && poetry run coverage report && poetry run coverage html
