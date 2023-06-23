00e055f2af2961119020906d3ef3e6cefc149fde
Replit user <>
2023-06-23 11:56:53 +0000

    Fix in typescript

--- cookiecutter/typescript/{{ cookiecutter.project_slug }}/spec/{{ cookiecutter.kata }}_spec.ts
+++ cookiecutter/typescript/{{ cookiecutter.project_slug }}/spec/{{ cookiecutter.kata }}_spec.ts
@@ -3,9 +3,9 @@
 import { expect, assert } from "chai"
 import { {{ cookiecutter.kata }} } from "../src/{{ cookiecutter.kata }}"
 
-describe("A new GameOfLife", function() {
+describe("A new {{ cookiecutter.kata }}", function() {
     it("is successfully created", function () {
-        var gameoflife  = new GameOfLife()
+        var my{{ cookiecutter.kata }}  = new {{ cookiecutter.kata }}()
         assert.equal(true, true)
         expect(false).to.be.false 
         expect(30).to.equal(30)
