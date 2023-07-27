e3debfd74ed98c0a09097a1eaa9ad061b1acbe50
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-07-28 01:29:13 +0200

    Fixed gilded rose python

--- tdd-katas/gilded-rose-kata/gilded-rose-python/replit.sh
+++
@@ -1,2 +0,0 @@
-#python -m http.server 8000 -d htmlcov >/dev/null 2>/dev/null &
-poetry run pytest && poetry run coverage run --branch --source gilded_rose gilded_rose_test.py && poetry run coverage report -m && poetry run coverage html
--- tdd-katas/gilded-rose-kata/gilded-rose-python/run.sh
+++
@@ -1,2 +0,0 @@
-echo "Running pytest with coverage..."
-pytest && coverage run --branch --source gilded_rose gilded_rose_test.py && coverage report -m && coverage html
