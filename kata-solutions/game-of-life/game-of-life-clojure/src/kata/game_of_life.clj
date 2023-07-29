a82357911696c0b0f4126c25921ab163e860cafc
Replit user <>
2023-07-29 11:52:56 +0000

    Development of game of life in Clojure

--- kata-solutions/game-of-life/game-of-life-clojure/src/kata/game_of_life.clj
+++ kata-solutions/game-of-life/game-of-life-clojure/src/kata/game_of_life.clj
@@ -1,11 +1,11 @@
 (ns kata.game-of-life
   (:gen-class))
 
-(defn living_cell 
+(defn living-cell 
   [x y]
   [x y true])
 
-(defn dead_cell 
+(defn dead-cell 
   [x y]
   [x y false])
 
@@ -16,3 +16,15 @@
 (defn alive?
   [cell]
   (= (last cell) true))
+
+(defn to-dead-cell
+  [cell-killable? cell]
+  (if (cell-killable? cell)
+    (dead-cell (get cell 0) (get cell 1))
+    cell))
+
+(defn to-living-cell
+  [cell-viable? cell]
+  (if (cell-viable? cell)
+    (living-cell (get cell 0) (get cell 1))
+    cell))
--- kata-solutions/game-of-life/game-of-life-clojure/test/kata/game_of_life_test.clj
+++ kata-solutions/game-of-life/game-of-life-clojure/test/kata/game_of_life_test.clj
@@ -2,18 +2,37 @@
   (:require [clojure.test :refer :all]
             [kata.game-of-life :refer :all]))
 
+(def LIVING-CELL (living-cell 0 0))
+(def DEAD-CELL (dead-cell 0 0))
+
 (deftest a-living-cell-is-alive
   (testing "Living cell should be alive."
-    (is (= true (alive? (living_cell 0 0))))))
+    (is (alive? LIVING-CELL))))
 
 (deftest a-dead-cell-is-not-alive
   (testing "Dead cell is not alive."
-    (is (= false (alive? (dead_cell 0 0))))))
+    (is (not (alive? DEAD-CELL)))))
 
 (deftest a-living-cell-is-not-dead
   (testing "Living cell is not dead."
-    (is (= false (dead? (living_cell 0 0))))))
+    (is (not (dead? LIVING-CELL)))))
 
 (deftest a-dead-cell-is-dead
   (testing "Dead cell is dead."
-    (is (= true (dead? (dead_cell 0 0))))))
+    (is (dead? DEAD-CELL))))
+
+(deftest map-living-cell-to-dead-cell
+  (testing "Map living cell to dead cell.")
+    (is (dead? (to-dead-cell alive? LIVING-CELL))))
+
+(deftest map-dead-cell-to-dead-cell
+  (testing "Map dead cell to dead cell.")
+    (is (dead? (to-dead-cell alive? DEAD-CELL))))
+
+(deftest map-dead-cell-to-living-cell
+  (testing "Map dead cell to living cell.")
+    (is (alive? (to-living-cell dead? DEAD-CELL))))
+
+(deftest map-living-cell-to-living-cell
+  (testing "Map living cell to living cell.")
+    (is (alive? (to-living-cell dead? LIVING-CELL))))
