053c8b6ee9fb31b25ab5117406110d91ca2bcd69
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-08-04 13:59:04 +0200

    Finishing free drinks in dotnet

--- kata-solutions/vending-machine/vending-machine-dotnet/VendingMachine.Tests/VendingMachineTest.cs
+++ kata-solutions/vending-machine/vending-machine-dotnet/VendingMachine.Tests/VendingMachineTest.cs
@@ -11,6 +11,8 @@ public class FreeDrinksVendingMachine : IDisposable
     public FreeDrinksVendingMachine()
     {
         vendingMachine = new VendingMachine();
+        vendingMachine.Configure(Choice.FIZZY_ORANGE, Can.FANTA);
+        vendingMachine.Configure(Choice.COLA, Can.COKE);
     }
 
     public void Dispose()
@@ -21,13 +23,18 @@ public class FreeDrinksVendingMachine : IDisposable
     [Fact]
     public void VendingMachineDeliversNothingWhenChoiceIsMade()
     {
-        Assert.Equal(Can.NOTHING, vendingMachine.Deliver(Choice.COLA));
+        Assert.Equal(Can.NOTHING, vendingMachine.Deliver(Choice.WATER));
     }
   
     [Fact]
     public void VendingMachineDeliversCokeWhenColaChoiceIsMade()
     {
-        vendingMachine.Configure(Choice.COLA, Can.COKE);
         Assert.Equal(Can.COKE, vendingMachine.Deliver(Choice.COLA));
+    }
+
+    [Fact]
+    public void VendingMachineDeliversFantaWhenFizzyOrangeChoiceIsMade()
+    {
+        Assert.Equal(Can.FANTA, vendingMachine.Deliver(Choice.FIZZY_ORANGE));
     }
 }
--- kata-solutions/vending-machine/vending-machine-dotnet/VendingMachine/VendingMachine.cs
+++ kata-solutions/vending-machine/vending-machine-dotnet/VendingMachine/VendingMachine.cs
@@ -6,25 +6,33 @@ namespace Kata;
 public enum Can : ushort
 {
     [Description("No can")] NOTHING,
+    [Description("Can of Fanta")] FANTA,
     [Description("Can of Coke")] COKE
 }
 
 public enum Choice : ushort
 {
-    [Description("Cola choice")] COLA
+    [Description("Cola choice")] COLA,
+    [Description("Fizzy orange choice")] FIZZY_ORANGE,
+    [Description("Water choice")] WATER
 }
 
 public class VendingMachine
 {
     private Can canOfChoice = Can.NOTHING;
-  
+    private Dictionary<Choice, Can> choiceCanMap = new Dictionary<Choice, Can>();
+
     public void Configure(Choice choice, Can can) 
     {
-        this.canOfChoice = can;
+        canOfChoice = can;
+        choiceCanMap[choice] = can;
     }
   
     public Can Deliver(Choice choice)
     {
-        return this.canOfChoice;
+        if (!choiceCanMap.ContainsKey(choice))
+            return Can.NOTHING;
+        
+        return choiceCanMap[choice];
     }
 }
