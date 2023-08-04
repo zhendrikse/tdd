2dc7a44a1d311311c72d28922927e35e0e28f5d0
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-08-04 16:28:21 +0200

    Shunting yard algo

---
+++ kata-solutions/shunting-yard-algo/shunting-yard-algo-dotnet/ShuntingYardAlgo.Tests/ShuntingYardAlgoTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System;
+
+namespace Kata.Tests;
+
+public class ShuntingYardAlgoTest
+{
+    [Fact]
+    public void TestShuntingYardAlgo()
+    {
+        Assert.True(false, "True should be true");
+    }
+}
---
+++ kata-solutions/shunting-yard-algo/shunting-yard-algo-dotnet/ShuntingYardAlgo/ShuntingYardAlgo.cs
@@ -0,0 +1,6 @@
+﻿namespace Kata;
+
+public class ShuntingYardAlgo
+{
+
+}
