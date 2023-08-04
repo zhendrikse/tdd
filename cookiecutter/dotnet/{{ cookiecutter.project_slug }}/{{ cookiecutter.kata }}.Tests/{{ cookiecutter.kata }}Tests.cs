710dbca4ff294ac9469b0347f9e223d02fae1c34
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-08-04 13:08:22 +0200

    Improvement dotnet cookiecutter template

---
+++ cookiecutter/dotnet/{{ cookiecutter.project_slug }}/{{ cookiecutter.kata }}.Tests/{{ cookiecutter.kata }}Tests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System;
+
+namespace Kata.Tests;
+
+public class {{ cookiecutter.kata }}Test
+{
+    [Fact]
+    public void Test{{ cookiecutter.kata }}()
+    {
+        Assert.True(false, "True should be true");
+    }
+}
