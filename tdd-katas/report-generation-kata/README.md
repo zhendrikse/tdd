0a96df7f0523eb6acd1cc62d501c8a7b1ce1a509
Replit user <>
2023-09-27 10:22:55 +0000

    Update readme

--- tdd-katas/report-generation-kata/README.md
+++ tdd-katas/report-generation-kata/README.md
@@ -2,3 +2,10 @@
 
 The report generation kata is based on 
 [this exercise](https://nbviewer.org/github/jerry-git/learn-python3/blob/master/notebooks/intermediate/exercises/05_idiomatic_python_exercise.ipynb).
+
+## Possible topics
+
+- SOLID
+- More Python idiomatic code
+- Clean code
+
--- tdd-katas/report-generation-kata/report-generation-python/test/report_generator_test.py
+++ tdd-katas/report-generation-kata/report-generation-python/test/report_generator_test.py
@@ -12,7 +12,7 @@ occurrences of most common: 4
 #####
 numbers: [1.0, 2.0, 99.0, 6.72, 2.0, 2.0, 2.0]
 words: ['john', 'doe', 'john', 'john', 'was', 'here', 'this', 'is', 'totally', 'random', 'john']"""
-    assert_that(ReportGenerator().get_report("test/data/misc_data1.txt"), equal_to(expected))
+    assert_that(ReportGenerator.get_report("test/data/misc_data1.txt"), equal_to(expected))
 
   def test_data_file_2(self):
     expected = """missing values: 3
@@ -22,7 +22,7 @@ occurrences of most common: 4
 #####
 numbers: [1.0, 2.0, 101.0, 6.72, 2.0, 2.0, 67.0, 2.0]
 words: ['john', 'doe', 'john', 'john', 'doe', 'was', 'doe', 'here', 'this', 'is', 'totally', 'random', 'john', 'doe']"""
-    assert_that(ReportGenerator().get_report("test/data/misc_data2.txt"), equal_to(expected))
+    assert_that(ReportGenerator.get_report("test/data/misc_data2.txt"), equal_to(expected))
 
   def test_empty_data_file(self):
     expected = """missing values: 0
@@ -32,5 +32,5 @@ occurrences of most common: 0
 #####
 numbers: []
 words: []"""
-    assert_that(ReportGenerator().get_report("test/data/empty.txt"), equal_to(expected))
+    assert_that(ReportGenerator.get_report("test/data/empty.txt"), equal_to(expected))
 
