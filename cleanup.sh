bb53b09164b066a8828db4066d1ee263e69858bb
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-03 18:06:45 +0200

    package-lock.json

--- cleanup.sh
+++ cleanup.sh
@@ -9,8 +9,6 @@ find . -name .coverage | xargs rm -rf
 find . -name coverage | xargs rm -rf
 find . -name __pycache__ | xargs rm -rf
 find . -name .pytest_cache | xargs rm -rf
-find . -name poetry.lock | xargs rm -rf
-find . -name package-lock.json | xargs rm -rf
 find . -name .lein-failures | xargs rm -rf
 rm -rf tdd-katas/train-reservation/train-reservation-dotnet/TicketOffice.Specs/Features/TrainCancellation.feature.cs
 rm -rf tdd-katas/train-reservation/train-reservation-dotnet/TicketOffice.Specs/Features/TrainReservation.feature.cs
