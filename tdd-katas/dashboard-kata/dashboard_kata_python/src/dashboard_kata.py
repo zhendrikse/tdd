8734ef31cc995a60666d7d7c89c93128fe12c216
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-04 05:35:33 +0000

    Update report kata

--- tdd-katas/dashboard-kata/dashboard_kata_python/src/dashboard_kata.py
+++ tdd-katas/dashboard-kata/dashboard_kata_python/src/dashboard_kata.py
@@ -2,14 +2,21 @@ import pandas as pnds
 # import plotly.express as plotly 
 import streamlit as strmlit
 
+col1 = "Q2"
+col2 = "Q4"
+col3 = "Dag_treinreis"
+col4 = "Q18D"
+col5 = "LeeftCat"
+col6 = "vertrekstation"
+col7 = "aankomststation"
 
 class Dashboard:
     def __init__(self, excel_file_name):
         self._init_webpage()
-        self._dataframe = self._init_dataframe(excel_file_name)
+        self._dataframe = self._read_data(excel_file_name)
 
-        if 'dataframe' not in strmlit.session_state:
-            strmlit.dataframe(self._dataframe)
+        # if 'dataframe' not in strmlit.session_state:
+        #     strmlit.dataframe(self._dataframe)
 
 
         if 'count' not in strmlit.session_state:
@@ -22,18 +29,19 @@ class Dashboard:
         # emoji's https://www.webfx.com/tools/emoji-cheat-sheet/
         strmlit.set_page_config(
             page_title = "Trein ritten dashboard", 
-            page_icon = ":train",
+            page_icon = ":train:",
             layout = "wide")
 
-    def _init_dataframe(self, excel_file_name):
-        return pnds.read_excel(
+    def _read_data(self, excel_file_name):
+        dataframe = pnds.read_excel(
             io = excel_file_name,
             engine = "openpyxl",
         #     sheet_name="NS Klimaat ritten - jan-mrt 202",
         #     skiprows="3",
-        #     usecols="Q59:Q4",
-            nrows = 10
+        #     usecols="Q2:Q4",
+            nrows = 20
         )
+        return dataframe[[col1, col2, col3, col4, col5, col6, col7]]
 
     def add(self):
         strmlit.session_state.count += 1
@@ -49,14 +57,50 @@ class Dashboard:
         with self.col2:
             strmlit.write(f"Hello from the {strmlit.session_state.tester}!")
 
+    def _define_filters(self):
+        strmlit.sidebar.header("Please filter here:")
+        Dag_treinreis = strmlit.sidebar.multiselect(
+            "Select the day:",
+            options = self._dataframe["Dag_treinreis"].unique(),
+            default = self._dataframe["Dag_treinreis"].unique()
+        )
+        Q2 = strmlit.sidebar.multiselect(
+            "Select gender:",
+            options = self._dataframe[col1].unique(),
+            default = self._dataframe[col1].unique()
+        )
+        Q4 = strmlit.sidebar.multiselect(
+            "Select days per week:",
+            options = self._dataframe[col2].unique(),
+            default = self._dataframe[col2].unique()
+        )
+        Q18D = strmlit.sidebar.multiselect(
+            "Select purpose:",
+            options = self._dataframe[col4].unique(),
+            default = self._dataframe[col4].unique()
+        )
+
+        self._dataframe = self._dataframe.query(
+            "Dag_treinreis == @Dag_treinreis & Q2 == @Q2 & Q4 == @Q4 & Q18D == @Q18D"
+        )
+
+        strmlit.dataframe(self._dataframe)
+
+    def cleanse(self):
+        self._dataframe = self._dataframe[self._dataframe[col1].isin(["Een jongen", "Een meisje"])]
+        strmlit.dataframe(self._dataframe)
+        
+
     def render(self):
+        self._define_filters()
+
         with self.col1:
             strmlit.button("Increment", on_click=self.add)
             strmlit.button("Subtract", on_click=self.subtract)
             strmlit.write(f'Count = {strmlit.session_state.count}')
         with self.col2:
             strmlit.button('test me', on_click=self.tester)
-
+            strmlit.button('Cleanse', on_click=self.cleanse)
 
 
 if __name__ == '__main__':
