6481d63343507c455e28047ee650e9041620f4b9
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-11 17:39:35 +0000

    Preparation for build

--- kata-solutions/game-of-life/game-of-life-python/run_tests.sh
+++ kata-solutions/game-of-life/game-of-life-python/run_tests.sh
@@ -1,2 +1,1 @@
-export PYTHONPATH=$PYTHONPATH:src:test
-poetry run mamba --format=documentation test/*_spec.py --enable-coverage && poetry run coverage html
+PYTHONPATH=$PYTHONPATH:src:test poetry run mamba --format=documentation test/*_spec.py --enable-coverage && poetry run coverage html
