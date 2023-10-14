fe73dc5312d2e080edba257e34ec4c1983d9d9d0
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-14 09:09:39 +0000

    Prepare for autobuild

--- kata-solutions/countries-kata/countries-kata-python/run_tests.sh
+++ kata-solutions/countries-kata/countries-kata-python/run_tests.sh
@@ -1,7 +1,1 @@
-export PYTHONPATH=$PYTHONPATH:src:test
- 
-poetry run ptw 
-#
-# TODO does not work yet with mamba
-#
-#ptw --runner "poetry run  pytest "
+PYTHONPATH=$PYTHONPATH:src:test poetry run pytest
