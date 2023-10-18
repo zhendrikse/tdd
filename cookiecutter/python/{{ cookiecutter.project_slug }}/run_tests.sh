4a08fd295a7530e7560762baaa476bf04a18de1b
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-10-18 16:01:27 +0000

    Added run_watch.sh

--- cookiecutter/python/{{ cookiecutter.project_slug }}/run_tests.sh
+++ cookiecutter/python/{{ cookiecutter.project_slug }}/run_tests.sh
@@ -1,8 +1,1 @@
-export PYTHONPATH=$PYTHONPATH:src:test
-{% if cookiecutter.rspec_syntax == "y" %}poetry run mamba --format=documentation test/*_test.py {% if cookiecutter.code_coverage == "y" %}--enable-coverage{% endif %}{% endif %} 
-{% if cookiecutter.rspec_syntax == "n" %}poetry run ptw {% if cookiecutter.code_coverage == "y" %}--runner="poetry run pytest --cov=. --cov-branch --cov-report html"{% endif %}{% endif %}
-
-#
-# TODO does not work yet with mamba
-#
-#ptw --runner "poetry run {% if cookiecutter.rspec_syntax == "y" %}mamba --format=documentation test/*_test.py {% if cookiecutter.code_coverage == "y" %}--enable-coverage{% endif %}{% endif %} {% if cookiecutter.rspec_syntax == "n" %}{% if cookiecutter.code_coverage == "n" %}pytest {% endif %}{% if cookiecutter.code_coverage == "y" %}coverage run -m pytest{% endif %}{% endif %}"
+PYTHONPATH=$PYTHONPATH:src:test {% if cookiecutter.rspec_syntax == "y" %}poetry run mamba --format=documentation test/*.py {% if cookiecutter.code_coverage == "y" %}--enable-coverage && poetry run coverage html{% endif %}{% endif %}{% if cookiecutter.rspec_syntax == "n" %}poetry run pytest{% if cookiecutter.code_coverage == "y" %} --cov{% endif %}{% endif %}
