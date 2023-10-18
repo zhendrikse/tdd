88fc24699fd575b292159ab3d3ed779be8b816af
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-18 12:54:53 +0000

    Code clean-up

--- kata-solutions/questionnaire-kata/questionnaire-python/src/excel_data_reader.py
+++ kata-solutions/questionnaire-kata/questionnaire-python/src/excel_data_reader.py
@@ -2,8 +2,8 @@ import pandas as pnds
 
 class ExcelDataReader:
     def __init__(self, excel_file_name):
-        self._variables = self._read_variable_sheet(excel_file_name)
-        self._ritten = self._read_ns_klimaat_ritten_sheet(excel_file_name)
+        self._variables = self._read_sheet(excel_file_name, "variabelen")
+        self._ritten = self._read_sheet(excel_file_name, "NS Klimaat ritten - jan-mrt 202")
 
     @property
     def ritten(self):
@@ -13,18 +13,10 @@ class ExcelDataReader:
     def variables(self):
         return self._variables
 
-    def _read_variable_sheet(self, excel_file_name):
+    def _read_sheet(self, excel_file_name, sheet_name):
         return pnds.read_excel(
             io = excel_file_name,
             engine = "openpyxl",
             header = 0,
-            sheet_name = "variabelen")
-
-    def _read_ns_klimaat_ritten_sheet(self, excel_file_name):
-        return pnds.read_excel(
-            io = excel_file_name,
-            engine = "openpyxl",
-            # sheet_name="NS Klimaat ritten - jan-mrt 202",
-            nrows = 200
-        )
+            sheet_name = sheet_name)
     
