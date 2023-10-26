a2fb32039d6b9e0f1030151db425d44200409a88
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2023-10-26 09:29:18 +0200

    Modifications for PyCharm

--- tdd-katas/tire-pressure-kata/tire-pressure-python/run_tests.sh
+++ tdd-katas/tire-pressure-kata/tire-pressure-python/run_tests.sh
@@ -1,7 +1,1 @@
-export PYTHONPATH=$PYTHONPATH:src:test
- 
-poetry run ptw 
-#
-# TODO does not work yet with mamba
-#
-#ptw --runner "poetry run  pytest "
+PYTHONPATH=src poetry run pytest
--- tdd-katas/video-store-kata/video-store-python/run_tests.sh
+++ tdd-katas/video-store-kata/video-store-python/run_tests.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=$PYTHONPATH:src:test poetry run pytest --cov
+PYTHONPATH=src poetry run pytest --cov
