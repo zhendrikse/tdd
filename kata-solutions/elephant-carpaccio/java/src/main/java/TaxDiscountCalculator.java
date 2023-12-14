1bbdee641a36efc3d2c80d4e7712b8a066fd806b
Zeger Hendrikse <zegerh@yahoo.co.uk>
2023-12-14 15:32:59 +0000

    First iteration

--- kata-solutions/elephant-carpaccio/java/src/main/java/TaxDiscountCalculator.java
+++ kata-solutions/elephant-carpaccio/java/src/main/java/TaxDiscountCalculator.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class TaxDiscountCalculator {
-    public String getGreeting() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("How many items: ");
+            Integer numberOfItems = scanner.nextInt();
+
+            System.out.print("Price item: ");
+            Float pricePerItem = scanner.nextFloat();
+
+            System.out.print("Two-letter state code: ");
+            String stateCode = scanner.nextLine();
+            
+
+            System.out.println("The total tax is: " + TaxDiscountCalculator.calculateTaxFor(numberOfItems, pricePerItem, stateCode));
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(new TaxDiscountCalculator().getGreeting());
+    private static String calculateTaxFor(Integer numberOfItems, Float pricePerItem, String stateCode) {
+        return "0";
     }
 }
