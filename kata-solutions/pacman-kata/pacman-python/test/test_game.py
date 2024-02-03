83483dfb870df69b775f384c11d15d0407ed94f2
Zeger Hendrikse <zeger.hendrikse@gmail.com>
2024-02-03 17:59:13 +0100

    Refactored tests

--- kata-solutions/pacman-kata/pacman-python/test/test_game.py
+++ kata-solutions/pacman-kata/pacman-python/test/test_game.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import List
 from hamcrest import is_, assert_that, has_length
 from game import Game
 from game_event import GameEvent, KeyPress
@@ -17,52 +18,23 @@ class TestGame:
     def screen_observer(self):
         self._screen_observer = FakeScreenObserver()
 
-    @pytest.fixture
-    def given_a_game_without_events(self):
+    def _given_a_game_with_events(self, events: List[List[GameEvent]]) -> Game:
         screen = FakeScreen(self._screen_observer)
-        event_bus = FakeEventBus([[QUIT_EVENT]])
+        event_bus = FakeEventBus(events)
         return Game(event_bus, FakeClock(), screen)
 
-    @pytest.fixture
-    def given_a_game_with_single_empty_tick(self):
-        screen = FakeScreen(self._screen_observer)
-        event_bus = FakeEventBus([[], [QUIT_EVENT]])
-        return Game(event_bus, FakeClock(), screen)
-
-    @pytest.fixture
-    def given_a_game_with_right_arrow_pressed_for_some_time(self):
-        screen = FakeScreen(self._screen_observer)
-        event_bus = FakeEventBus([
-                [GameEvent(keypress=KeyPress.ARROW_RIGHT_PRESSED)],
-                [],
-                [],
-                [GameEvent(keypress=KeyPress.ARROW_RIGHT_RELEASED)],
-                [],
-                [QUIT_EVENT]])
-        return Game(event_bus, FakeClock(), screen)
-
-    @pytest.fixture
-    def given_a_game_with_left_arrow_pressed_for_some_time(self):
-        screen = FakeScreen(self._screen_observer)
-        event_bus = FakeEventBus([
-                [GameEvent(keypress=KeyPress.ARROW_LEFT_PRESSED)],
-                [],
-                [],
-                [GameEvent(keypress=KeyPress.ARROW_RIGHT_RELEASED)],
-                [],
-                [QUIT_EVENT]])
-        return Game(event_bus, FakeClock(), screen)
-
-    def test_game_without_events_draws_sprite(self, given_a_game_without_events):
-        given_a_game_without_events.run()
+    def test_game_without_events_draws_sprite(self):
+        events = [[QUIT_EVENT]]
+        self._given_a_game_with_events(events).run()
 
         assert_that(self._screen_observer.messages, has_length(3))
         assert_that(self._screen_observer.messages[0], is_('Circle with radius 10 rendered at <50, 50>'))
         assert_that(self._screen_observer.messages[1], is_('refresh'))
         assert_that(self._screen_observer.messages[2], is_('quit'))
 
-    def test_second_tick_leaves_sprite_position_unchanged(self, given_a_game_with_single_empty_tick):
-        given_a_game_with_single_empty_tick.run()
+    def test_second_tick_leaves_sprite_position_unchanged(self):
+        events = [[], [QUIT_EVENT]]
+        self._given_a_game_with_events(events).run()
 
         assert_that(self._screen_observer.messages, has_length(5))
         assert_that(self._screen_observer.messages[0], is_('Circle with radius 10 rendered at <50, 50>'))
@@ -71,8 +43,15 @@ class TestGame:
         assert_that(self._screen_observer.messages[3], is_('refresh'))
         assert_that(self._screen_observer.messages[4], is_('quit'))
 
-    def test_move_sprite_to_the_right(self, given_a_game_with_right_arrow_pressed_for_some_time):
-        given_a_game_with_right_arrow_pressed_for_some_time.run()
+    def test_move_sprite_to_the_right(self):
+        events = [
+            [GameEvent(keypress=KeyPress.ARROW_RIGHT_PRESSED)],
+            [],
+            [],
+            [GameEvent(keypress=KeyPress.ARROW_RIGHT_RELEASED)],
+            [],
+            [QUIT_EVENT]]
+        self._given_a_game_with_events(events).run()
 
         assert_that(self._screen_observer.messages, has_length(13))
         assert_that(self._screen_observer.messages[0], is_('Circle with radius 10 rendered at <50, 50>'))
@@ -89,8 +68,15 @@ class TestGame:
         assert_that(self._screen_observer.messages[11], is_('refresh'))
         assert_that(self._screen_observer.messages[12], is_('quit'))
 
-    def test_move_sprite_to_the_left(self, given_a_game_with_left_arrow_pressed_for_some_time):
-        given_a_game_with_left_arrow_pressed_for_some_time.run()
+    def test_move_sprite_to_the_left(self):
+        events = [
+            [GameEvent(keypress=KeyPress.ARROW_LEFT_PRESSED)],
+            [],
+            [],
+            [GameEvent(keypress=KeyPress.ARROW_LEFT_RELEASED)],
+            [],
+            [QUIT_EVENT]]
+        self._given_a_game_with_events(events).run()
 
         assert_that(self._screen_observer.messages, has_length(13))
         assert_that(self._screen_observer.messages[0], is_('Circle with radius 10 rendered at <50, 50>'))
