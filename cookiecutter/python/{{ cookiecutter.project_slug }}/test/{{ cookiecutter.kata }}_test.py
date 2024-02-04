8a474cf2c9440f611ad8f2d9b1b2621a0455f5e6
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-02-04 18:51:25 +0100

    Name of test in lowercase

--- cookiecutter/python/{{ cookiecutter.project_slug }}/test/{{ cookiecutter.kata }}_test.py
+++ cookiecutter/python/{{ cookiecutter.project_slug }}/test/{{ cookiecutter.kata }}_test.py
@@ -5,7 +5,7 @@ from {{ cookiecutter.kata }} import {{ cookiecutter.kata }}
 
 class Test{{ cookiecutter.kata }}:
 
-  def test_a_new_{{ cookiecutter.kata }}(self):
+  def test_a_new_{{ cookiecutter.project_slug }}(self):
       assert True is False
       # Hamcrest style
       # assert_that(True, equal_to(False))
