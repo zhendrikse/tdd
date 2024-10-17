856280535d7c3584f5ccb50b75ea1efd275929b2
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-10-17 13:05:15 +0000

    Added solution to Javascript version

---
+++ kata-solutions/gilded-rose-kata/gilded-rose-javascript/4-gilded-rose-extended/src/gilded_rose.js
@@ -0,0 +1,124 @@
+class Item {
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+  updateQualityAfterSellIn() {
+    if (this.quality > 0)
+      this.quality = this.quality - 1;
+  }
+
+  updateQualityBeforeSellIn() {
+    this.decrementQuality();
+  }
+
+  decrementQuality() {
+    if (this.quality > 0)
+      this.quality -= 1;
+  }
+
+  incrementQuality() {
+    if (this.quality < 50)
+      this.quality = this.quality + 1;
+  }
+
+  updateProduct() {
+    this.updateQualityBeforeSellIn();
+    this.sellIn = this.sellIn - 1;
+    this.updateQualityAfterSellIn();
+  }
+
+  toString() {
+    return "name=\"" + this.name + "\", sellIn=\"" + this.sellIn + "\", quality=\"" + this.quality + "\""
+  }
+}
+
+class BackstagePass extends Item {
+  constructor(sellIn, quality) {
+    super('Backstage passes to a TAFKAL80ETC concert', sellIn, quality);
+  }
+
+  updateQualityBeforeSellIn() {
+    if (this.quality < 50) {
+      this.quality = this.quality + 1;
+      if (this.sellIn < 11) 
+        super.incrementQuality();
+      if (this.sellIn < 6) 
+        super.incrementQuality();
+    }
+  }
+
+  updateQualityAfterSellIn() {
+    if (this.sellIn < 0) 
+      this.quality = 0;
+  }
+}
+
+class AgedBrie extends Item {
+  constructor(sellIn, quality) {
+    super('Aged Brie', sellIn, quality);
+  }
+
+  updateQualityBeforeSellIn() {
+    super.incrementQuality();
+  }
+
+  updateQualityAfterSellIn() {
+    super.incrementQuality();
+  }
+}
+
+class Sulfuras extends Item {
+  constructor(sellIn, quality) {
+    super('Sulfuras, Hand of Ragnaros', sellIn, quality);
+  }
+
+  updateProduct() {
+  }
+}
+
+class ConjuredItem extends Item {
+  constructor(sellIn, quality) {
+    super("Conjured", sellIn, quality);
+  }
+
+  updateQualityBeforeSellIn() {
+    if (this.sellIn == 0) {
+      super.decrementQuality();
+      super.decrementQuality();
+    }
+
+    if (this.sellIn == 5) 
+      this.decrementQuality();
+    
+    super.decrementQuality();
+  }
+
+  updateQualityAfterSellIn() {
+    super.decrementQuality();
+  }
+}
+
+class Shop {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    for (var i = 0; i < this.items.length; i++)
+      this.items[i].updateProduct();
+
+    return this.items;
+  }
+}
+
+module.exports = {
+  Item,
+  Sulfuras, 
+  BackstagePass,
+  AgedBrie,
+  ConjuredItem,
+  Shop
+}
