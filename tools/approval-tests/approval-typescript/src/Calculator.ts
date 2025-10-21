20dce3748a3f8b2f9116e95d019c7405d99bb2d1
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-10-21 21:36:35 +0200

    Updated approval test example

---
+++ tools/approval-tests/approval-typescript/src/Calculator.ts
@@ -0,0 +1,5 @@
+export class Calculator {
+  public add(x: number, y: number): number {
+    return x + y
+  }
+}
--- tools/approval-tests/approval-typescript/test/sample.test.ts
+++ tools/approval-tests/approval-typescript/test/sample.test.ts
@@ -3,6 +3,7 @@ import { beforeAll, describe, expect, test } from "@jest/globals";
 import {verify, verifyAsJson} from "approvals/lib/Providers/Jest/JestApprovals";
 import {configure} from "approvals/lib/config";
 import {JestReporter} from "approvals/lib/Providers/Jest/JestReporter";
+import { Calculator } from "../src/Calculator"
 
 
 describe("ApprovalTests", () => {
@@ -11,11 +12,20 @@ describe("ApprovalTests", () => {
       reporters: [new JestReporter()],
     });
   });
+
    test("SimpleVerify", () => {
      verify("Hello From Approvals");
    });
+
   test("JsonVerify", () => {
     const data = {name:"fred", age: 30}
     verifyAsJson(data);
+  });
+
+  test("A calculator can add two numbers", () => {
+    const myCalculator  = new Calculator()
+    const result = myCalculator.add(3, 4)
+    // expect(result).toEqual(7)
+    verify(result)
   });
 });
